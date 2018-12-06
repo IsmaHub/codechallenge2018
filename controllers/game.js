fe93973e3cd2fea221c26225917d6a20125dde7f
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2018-12-06 18:14:06 +0100

    minor changes

--- controllers/game.js
+++ controllers/game.js
@@ -16,6 +16,7 @@ module.exports = {
                     }
                 }
             }
+            console.log("DIRECTION TARGET: "+directionString)
             return directionString
         }
         
@@ -35,6 +36,7 @@ module.exports = {
                     }else objectResponse.right += 1
                 }
             });
+            console.log("FIRE TARGET: "+objectResponse)
             return objectResponse
         }
 
