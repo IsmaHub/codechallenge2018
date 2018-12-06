7d4bb03dafe04379f5b0f68d125d9cf71b2322ae
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2018-12-06 15:39:35 +0100

    added basic response

--- controllers/game.js
+++ controllers/game.js
@@ -1,9 +1,17 @@
 module.exports = {
     move: (req, res) => {
+        const data = req.body
+        const currentPosition = req.body.player.position
+        const previousPosition = req.body.player.previous
+        const visibleArea = req.body.player.area
+        const fire = req.body.player.fire
+
+
+        return res.send({move: ""}).end();
 
     },
 
-    name: (req, res) =>{
-        
+    name: (req, res) => {
+        return res.send({email: "ismael.jimenez@ext.privalia.com", name: "ENP"}).end();
     }
 }
