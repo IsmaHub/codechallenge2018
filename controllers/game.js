1ba920a37557e087e7da365d09a1801ac5e65d2d
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2018-12-06 17:59:13 +0100

    minor changes

--- controllers/game.js
+++ controllers/game.js
@@ -13,40 +13,47 @@ module.exports = {
         if(directionString.length){
             return res.send({move: "fire-"+directionString}).end();
         }
+
+        /**
+         * 
+         */
+        _getFireDirection = (targets) => {
+            let directionString = "";
+            let lastDirection = 0;
+            for(var direction in targets) {
+                if(targets.hasOwnProperty(direction)) {
+                    if(targets[direction] > lastDirection){
+                        directionString = direction
+                        lastDirection = targets[direction]
+                    }
+                }
+            }
+            return directionString
+        }
+        
+        /**
+         * 
+         */
+        _getFireTargets = (targets, pos) => {
+            let objectResponse = {}
+            targets.forEach(target => {
+                if(target.x === pos.x){
+                    if(target.y > pos.y){
+                        objectResponse.down += 1
+                    }else objectResponse.up += 1
+                }else if(target.y === pos.y){
+                    if(target.x > pos.x){
+                        objectResponse.left += 1
+                    }else objectResponse.right += 1
+                }
+            });
+            return objectResponse
+        }
+        
         return res.send({move: "up"}).end();
     },
 
     name: (req, res) => {
         return res.send({email: "ismael.jimenez@ext.privalia.com", name: "ENP"}).end();
-    },
-
-    _getFireTargets: (targets, pos) => {
-        let objectResponse = {}
-        targets.forEach(target => {
-            if(target.x === pos.x){
-                if(target.y > pos.y){
-                    objectResponse.down += 1
-                }else objectResponse.up += 1
-            }else if(target.y === pos.y){
-                if(target.x > pos.x){
-                    objectResponse.left += 1
-                }else objectResponse.right += 1
-            }
-        });
-        return objectResponse
-    },
-
-    _getFireDirection: (targets) => {
-        let directionString = "";
-        let lastDirection = 0;
-        for(var direction in targets) {
-            if(targets.hasOwnProperty(direction)) {
-                if(targets[direction] > lastDirection){
-                    directionString = direction
-                    lastDirection = targets[direction]
-                }
-            }
-        }
-        return directionString
     }
 }
