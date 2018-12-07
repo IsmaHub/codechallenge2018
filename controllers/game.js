0e34945cd433706e5bdbd866559db8923c79641c
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2018-12-07 21:19:35 +0100

    next move changes

--- controllers/game.js
+++ controllers/game.js
@@ -20,25 +20,29 @@ module.exports = {
                 !_isWall(data.player.position.x, data.player.position.y-1) &&
                 _isWall(data.player.position.x-1, data.player.position.y) &&
                 (
-                    (data.player.position.x !== data.player.previous.x || data.player.position.y-1 !== data.player.previous.y)
+                    (data.player.position.x !== data.player.previous.x || data.player.position.y-1 !== data.player.previous.y) &&
+                    (data.player.position.x-1 == data.player.previous.x && data.player.position.y == data.player.previous.y)
                 )
             ){
                 return "up"
             //RIGHT
             }else if(
                 !_isWall(data.player.position.x+1, data.player.position.y) &&
-                (data.player.position.x !== data.player.previous.x || data.player.position.y+1 !== data.player.previous.y) &&
+                _isWall(data.player.position.x, data.player.position.y-1) &&
                 (data.player.position.x+1 !== data.player.previous.x || data.player.position.y !== data.player.previous.y)
             ){
                 return "right"
             //DOWN
             }else if(
                 !_isWall(data.player.position.x, data.player.position.y+1) &&
+                _isWall(data.player.position.x+1, data.player.position.y) &&
+                _isWall(data.player.position.x, data.player.position.y-1) &&
                 (data.player.position.x !== data.player.previous.x || data.player.position.y+1 !== data.player.previous.y)
             ){
                 return "down"
             //LEFT
             }else if(
+                _isWall(data.player.position.x, data.player.position.y+1) &&
                 !_isWall(data.player.position.x-1, data.player.position.y) &&
                 (data.player.position.x-1 !== data.player.previous.x || data.player.position.y !== data.player.previous.y)
             ){
