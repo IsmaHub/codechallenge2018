53fbfaab62ec7909ced78098e06f9db6fdc15645
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2018-12-07 21:28:47 +0100

    wip

--- controllers/game.js
+++ controllers/game.js
@@ -18,7 +18,6 @@ module.exports = {
             //UP
             if(
                 !_isWall(data.player.position.x, data.player.position.y-1) &&
-                _isWall(data.player.position.x-1, data.player.position.y) &&
                 (
                     (data.player.position.x !== data.player.previous.x || data.player.position.y-1 !== data.player.previous.y) &&
                     (data.player.position.x-1 == data.player.previous.x && data.player.position.y == data.player.previous.y)
